df1a2a187ebfc1cf8a074ea75da82c0bc919717b
Stella <stelli@cmc.edu>
2020-05-02 16:12:49 -0700

    fixxxx

--- Trees/Heap.py
+++ Trees/Heap.py
@@ -195,7 +195,7 @@ class Heap(BinaryTree):
     @staticmethod
 
     def _remove_last_node(node):
-        binary = '{0:b}'.format(node.descendents)
+        binary = "{0:b}".format(node.descendents)
         node.descendents = node.descendents -1 
         if len(binary) ==2:
             if binary[1] =='1':
